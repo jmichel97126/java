a7375fcf92e0f75d63f24e1b86f6cd7a9b6830cd
jmichel97126 <52572106+jmichel97126@users.noreply.github.com>
2019-07-05 16:20:43 +0200

    Depot projet

---
+++ scoreboard/src/main/java/fr/jmichel/kata/tennis/bean/Player.java
@@ -0,0 +1,54 @@
+package fr.jmichel.kata.tennis.bean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.jmichel.kata.tennis.tool.ScorePoint;
+
+public class Player {
+	
+	private int id;
+	private List<Integer> score;
+	private boolean winner;
+	private boolean advantage;
+
+	public Player(int id) {
+			this.id = id;
+			score = new ArrayList<Integer>();
+			score.add(ScorePoint.SCORE_0);
+			winner = false;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public List<Integer> getScore() {
+		return score;
+	}
+
+	public void setScore(List<Integer> score) {
+		this.score = score;
+	}
+
+	public boolean isWinner() {
+		return winner;
+	}
+
+	public void setWinner(boolean winner) {
+		this.winner = winner;
+	}
+
+	public boolean isAdvantage() {
+		return advantage;
+	}
+
+	public void setAdvantage(boolean advantage) {
+		this.advantage = advantage;
+	}
+
+}
